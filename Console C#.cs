95d9e880377456f0105d5895b57b7deac5ad341e
King <gyattohio4444@gmail.com>
2024-08-05 13:39:28 -0400

    Console C#

---
+++ Console C#.cs
@@ -0,0 +1,20 @@
+// Console Code
+// Made By King With <3 Love <3
+// Basic
+Console.Title ""; // Changes The Title
+Console.ForegroundColor = ConsoleColor.White // Changes The Text Color
+Console.Background = ConsoleColor.Black // Chnages THe Background Color
+Console.WriteLine(); // Writes A Blank Line
+Console.WriteLine(""); // Writes A Line 
+Console.Write(""); // Writes A Line
+
+// advanced
+string Text = ""; // String That Holds Text
+Console.WriteLine(Text); // Pasts The Text In The String Thing
+Console.ReadLine(); // Reads What The Outout From A Console.WriteLine() Was
+Console.WriteLine(What Your Name: ); // Asks For A Output
+string Name = Console.ReadLine(); // Takes A Output
+if (Name == "") // If THe Output Is What You Put
+{
+
+}
